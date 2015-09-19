7039c43aa38e08e606eaa3db03dad33cc4c3440a
rebornwhu <rebornwhu@gmail.com>
2015-09-19 16:37:33 -0700

    Apply style to title

--- ios-button-fun/ViewController.swift
+++ ios-button-fun/ViewController.swift
@@ -13,7 +13,12 @@ class ViewController: UIViewController {
     @IBAction func buttonPressed(sender: UIButton) {
         let title = sender.titleForState(.Normal)!
         let plainText = "\(title) button pressed"
-        statusLabel.text = plainText
+        let styledText = NSMutableAttributedString(string: plainText)
+        let attributes = [NSFontAttributeName:UIFont.boldSystemFontOfSize(statusLabel.font.pointSize)]
+        let nameRange = (plainText as NSString).rangeOfString(title)
+        styledText.setAttributes(attributes, range: nameRange)
+
+        statusLabel.attributedText = styledText
     }
     
     @IBOutlet weak var statusLabel: UILabel!
